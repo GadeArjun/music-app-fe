4297a49d550d626429ba4409e24a29a388c202ce
Arjun Gade <139616230+gadearjun24@users.noreply.github.com>
2024-08-10 18:05:46 +0000

    music-app

--- music-app/src/components/Music.js
+++ music-app/src/components/Music.js
@@ -7,12 +7,17 @@ function Music() {
         <div className="current-music-detail">
           <div className="current-music-image">
             <img
-              src="https://i.ytimg.com/vi/gPpQNzQP6gE/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLArGxYrlrz3f-B8etBZ1B9WlO93mA"
+              src="https://i.ytimg.com/vi/nFgsBxw-zWQ/hq720.jpg"
               alt="music-image"
             />
           </div>
 
-          <div className="current-music-info"></div>
+          <div className="current-music-info">
+            <p>
+             <span className="current-music-tag">Title : </span> Aayi Nai -Stree 2 | Shraddha Kapoor | Rajkummar Rao |
+              Sachin-Jigar |Pawan Singh,Simran,Divya,Amitabh
+            </p>
+          </div>
         </div>
       </div>
     </>
