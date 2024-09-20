57539320da03158a5e23af5a9efe83c550297f91
Arjun Gade <139616230+gadearjun24@users.noreply.github.com>
2024-09-20 18:26:00 +0000

    music-app

--- music-app/src/components/Search.js
+++ music-app/src/components/Search.js
@@ -5,9 +5,35 @@ import NavigationBar from "./NavigationBar";
 import { useNavigate } from "react-router-dom";
 
 function Search() {
-  const [searchMusicData, setSearchMusicData] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [recent , setRecent] = useState(false);
+  const [searchMusicData, setSearchMusicData] = useState([
+    {
+      thumbnailUrl:
+        "https://t4.ftcdn.net/jpg/04/10/17/95/240_F_410179527_ExxSzamajaCtS16dyIjzBRNruqlU5KMA.jpg",
+      title: "Title is Loading",
+    },
+    {
+      thumbnailUrl:
+        "https://t4.ftcdn.net/jpg/04/10/17/95/240_F_410179527_ExxSzamajaCtS16dyIjzBRNruqlU5KMA.jpg",
+      title: "Title is Loading",
+    },
+    {
+      thumbnailUrl:
+        "https://t4.ftcdn.net/jpg/04/10/17/95/240_F_410179527_ExxSzamajaCtS16dyIjzBRNruqlU5KMA.jpg",
+      title: "Title is Loading",
+    },
+    {
+      thumbnailUrl:
+        "https://t4.ftcdn.net/jpg/04/10/17/95/240_F_410179527_ExxSzamajaCtS16dyIjzBRNruqlU5KMA.jpg",
+      title: "Title is Loading",
+    },
+    {
+      thumbnailUrl:
+        "https://t4.ftcdn.net/jpg/04/10/17/95/240_F_410179527_ExxSzamajaCtS16dyIjzBRNruqlU5KMA.jpg",
+      title: "Title is Loading",
+    },
+  ]);
+  const [loading, setLoading] = useState(true);
+  const [recent, setRecent] = useState(false);
   const navigate = useNavigate();
   useEffect(() => {
     async function fetchData() {
@@ -16,7 +42,7 @@ function Search() {
         const res = await axios.get("/searchsongs/");
         setSearchMusicData(res.data);
         setLoading(false);
-        setRecent(true)
+        setRecent(true);
       } catch (err) {
         console.log(err);
       }
@@ -31,47 +57,54 @@ function Search() {
   if (searchMusicData.length === 0 || loading) {
     return (
       <>
-        <NavigationBar />
+        <NavigationBar
+          setSearchMusicData={setSearchMusicData}
+          setLoading={setLoading}
+          setRecent={setRecent}
+        />
         <SearchLoading />
       </>
     );
-  }
+  } else {
+    return (
+      <>
+        <NavigationBar
+          setSearchMusicData={setSearchMusicData}
+          setLoading={setLoading}
+          setRecent={setRecent}
+        />
+        <h2 className="loading">
+          {`${recent ? "Recent search result..." : "Your search result..."}`}
+        </h2>
 
-  return (
-    <>
-      <NavigationBar
-        setSearchMusicData={setSearchMusicData}
-        setLoading={setLoading}
-        setRecent = {setRecent}
-      />
-
-<h2 className="loading"> {`${recent ? "Recent search result..." : "Your search result..."}`}</h2>
-
-      <div className="loading-container">
-        <div className="all-loading-music">
-          {searchMusicData.map((ele, index) => {
-            return (
-              <>
-                <div
-                  className="loading-music"
-                  onClick={() => handleSearchMusicClick(index + 1)}
-                >
-                  <div className="loading-music-image">
-                    <img src={ele.thumbnailUrl} alt={`music-image`} />
-                  </div>
-                  <div className="loading-music-title">
-                    <p>
-                      <span>Title : </span> {ele.title}
-                    </p>
-                  </div>
-                </div>
-              </>
-            );
-          })}
+        <div className="loading-container">
+          <div className="all-loading-music">
+            {!loading
+              ? searchMusicData.map((ele, index) => {
+                  return (
+                    <>
+                      <div
+                        className="loading-music"
+                        onClick={() => handleSearchMusicClick(index + 1)}
+                      >
+                        <div className="loading-music-image">
+                          <img src={ele.thumbnailUrl} alt={`music-image`} />
+                        </div>
+                        <div className="loading-music-title">
+                          <p>
+                            <span>Title : </span> {ele.title}
+                          </p>
+                        </div>
+                      </div>
+                    </>
+                  );
+                })
+              : ""}
+          </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
 }
 
 export default Search;
